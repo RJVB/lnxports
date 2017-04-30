358982441adb8dcdd48d40ee21e34f822ee032be
René J.V. Bertin <rjvbertin@gmail.com>
2017-04-30 15:41:08 +0200

    port:pkg-config : take share/pkgconfig dir into consideration
    Committed from host : Bola

--- devel/pkgconfig/files/pkg-config.sh
+++ devel/pkgconfig/files/pkg-config.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 if [ "${PKG_CONFIG_MACPORTS_ONLY}" = "" ] ;then
-	if [ "${PKG_CONFIG}" = "" ] ;then
-		PKG_CONFIG="/usr/bin/pkg-config"
+	if [ "${PKG_CONFIG}" = "" -o "${PKG_CONFIG}" = $0 ] ;then
+		PKG_CONFIG_SYS="/usr/bin/pkg-config"
+	else
+		PKG_CONFIG_SYS="${PKG_CONFIG}"
 	fi
 	if [ "${PKG_CONFIG_PATH}" != "" ] ;then
-		PKG_CONFIG_PATH="@PREFIX@/lib/pkgconfig:@PREFIX@/lib/@BUILDARCH@-linux-gnu/pkgconfig:${PKG_CONFIG_PATH}"
+		PKG_CONFIG_PATH="@PREFIX@/lib/pkgconfig:@PREFIX@/share/pkgconfig:@PREFIX@/lib/@BUILDARCH@-linux-gnu/pkgconfig:${PKG_CONFIG_PATH}"
 	else
-		PKG_CONFIG_PATH="@PREFIX@/lib/pkgconfig:@PREFIX@/lib/@BUILDARCH@-linux-gnu/pkgconfig"
+		PKG_CONFIG_PATH="@PREFIX@/lib/pkgconfig:@PREFIX@/share/pkgconfig:@PREFIX@/lib/@BUILDARCH@-linux-gnu/pkgconfig"
 	fi
 	export PKG_CONFIG_PATH
-	exec "${PKG_CONFIG}" "$@"
+	exec "${PKG_CONFIG_SYS}" "$@"
 else
 	exec pkg-config-mp "$@"
 fi
