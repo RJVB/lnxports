b1c48bd3a59621bc5d31e52fae32cfa875024968
René Bertin <rjvbertin@gmail.com>
2020-04-05 11:42:52 +0200

    PortGroups: sync with MacStrop
    Committed from host : Bola
    
    Committed from host : Bola

--- _resources/port1.0/group/kf5-1.1.tcl
+++ _resources/port1.0/group/kf5-1.1.tcl
@@ -69,6 +69,15 @@ if {${kf5::includecounter} == 0} {
     }
 }
 
+if {${os.platform} eq "darwin" && ${os.major} <= 13} {
+    depends_fetch-append \
+                    bin:wget:wget
+    # avoid certificate issues on kde.org
+    fetch {
+        system "wget ${master_sites}/${distname}${extract.suffix} --no-check-certificate --progress=bar:force -O ${distpath}/${distname}${extract.suffix}"
+    }
+}
+
 ########################################################################
 # Projects including the 'kf5' port group can optionally set
 #
@@ -522,6 +531,9 @@ proc kf5.set_project {project} {
         }
         distname            ${project}-${kf5.version}.git
     } else {
+#         See
+#         https://kde.org/info/releases-19.12.1.php
+#         for new/different download locations for Applications 19.12.1 (and future) releases.
         master_sites        https://download.kde.org/${dbranch}/${f}
     }
 }
