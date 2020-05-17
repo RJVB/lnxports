4b2c8d1a50acf89c46227f3da39248b469f6f755
René Bertin <rjvbertin@gmail.com>
2020-05-17 11:20:45 +0200

    python PG : can't leave comments in Tcl switch statement
    Committed from host : Bola

--- _resources/port1.0/group/python-1.0.tcl
+++ _resources/port1.0/group/python-1.0.tcl
@@ -293,25 +293,6 @@ default destroot.destdir {--prefix=${python.prefix} --root=${destroot}}
 proc python_get_defaults {var} {
     global python.version python.branch prefix python.prefix os.platform
     switch -- $var {
-# tentative removal during upstream sync, 20200516
-#         prefix {
-#             global build_arch frameworks_dir
-#             set ret "${frameworks_dir}/Python.framework/Versions/${python.branch}"
-#             if {${python.version} == 25 || (${python.version} == 24 &&
-#                 ![file isfile ${ret}/include/python${python.branch}/Python.h] &&
-#                 ([file isfile ${prefix}/include/python${python.branch}/Python.h]
-#                 || [string match *64* $build_arch]))} {
-#                 set ret $prefix
-#             }
-#             return $ret
-#         }
-#         bin {
-#             if {${os.platform} eq "darwin" && ${python.version} != 24} {
-#                 return "${python.prefix}/bin/python${python.branch}"
-#             } else {
-#                 return "${prefix}/bin/python${python.branch}"
-#             }
-#         }
         include {
             set inc_dir "${python.prefix}/include/python${python.branch}"
             if {[file exists ${inc_dir}]} {
@@ -332,14 +313,6 @@ proc python_get_defaults {var} {
                 }
             }
         }
-# tentative removal during upstream sync, 20200516
-#         lib {
-#             if {${python.version} != 24 && ${python.version} != 25} {
-#                 return "${python.prefix}/Python"
-#             } else {
-#                 return "${prefix}/lib/lib${python.branch}.so"
-#             }
-#         }
         pkgd {
             if {${python.version} != 24} {
                 return "${python.prefix}/lib/python${python.branch}/site-packages"
@@ -347,35 +320,6 @@ proc python_get_defaults {var} {
                 return "${prefix}/lib/python${python.branch}/site-packages"
             }
         }
-# tentative removal during upstream sync, 20200516
-#         setup_args {
-#             if {${python.version} != 24} {
-#                 return "--no-user-cfg"
-#             } else {
-#                 return ""
-#             }
-#         }
-#         setup_prefix {
-#             if {${python.version} != 24} {
-#                 return "${python.prefix}"
-#             } else {
-#                 return "${prefix}"
-#             }
-#         }
-#         link_binaries {
-#             if {${python.version} != 24 && ${python.version} != 25} {
-#                 return yes
-#             } else {
-#                 return no
-#             }
-#         }
-#         move_binaries {
-#             if {${python.version} == 24 || ${python.version} == 25} {
-#                 return yes
-#             } else {
-#                 return no
-#             }
-#         }
         binary_suffix {
             if {[string match py-* [option name]]} {
                 return -${python.branch}
