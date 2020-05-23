059026659a20bf367d8cada6070535a07e37e1d8
René Bertin <rjvbertin@gmail.com>
2020-05-23 15:31:00 +0200

    port:vala : new port
    Committed from host : Bola

---
+++ lang/vala/files/valac.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+VAPIDIRS=""
+for J in @PREFIX@/share/vala*/vapi /usr/share/vala*/vapi ;do
+    VAPIDIRS="${VAPIDIRS} --vapidir=${J}"
+done
+GIRDIRS=""
+for J in @PREFIX@/share/gir-1.0 /usr/share/gir-1.0 ;do
+    VAPIDIRS="${VAPIDIRS} --girdir=${J}"
+done
+
+exec @PREFIX@/bin/vala-@VERSION@ "$@" ${VAPIDIRS} ${GIRDIRS}
