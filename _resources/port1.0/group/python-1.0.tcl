cb1ad67557144da3fbb897bfb0dfff246e1f3d49
René Bertin <rjvbertin@gmail.com>
2022-01-04 21:06:00 +0100

    port:python37 : fix python PG : sync with upstream changes (v2.7.0-archive-10423-g2ef21486e52)
    Committed from host : Bola

--- _resources/port1.0/group/python-1.0.tcl
+++ _resources/port1.0/group/python-1.0.tcl
@@ -17,19 +17,21 @@
 #
 # Note: setting these options requires name to be set beforehand
 
+PortGroup       compiler_wrapper 1.0
+
 categories      python
 
 use_configure   no
+
 # we want the default universal variant added despite not using configure
 universal_variant yes
-
-default build.target {build[python_get_defaults jobs_arg]}
 
 post-extract {
     # Prevent setuptools' easy_install from downloading dependencies
     set fs [open $env(HOME)/.pydistutils.cfg w+]
     puts $fs {[easy_install]}
-    puts $fs {allow_hosts = None}
+    puts $fs {index_url = ''}
+    puts $fs {find_links = ''}
     close $fs
     # Same for pip
     file mkdir $env(HOME)/.config/pip
@@ -40,7 +42,7 @@ post-extract {
     close $fs
 }
 
-pre-destroot    {
+pre-destroot {
     xinstall -d -m 0755 ${destroot}${prefix}/share/doc/${subport}/examples
 }
 
@@ -57,15 +59,15 @@ default distname        {${python.rootname}-${version}}
 options python.versions python.version python.default_version
 option_proc python.versions python_set_versions
 default python.default_version {[python_get_default_version]}
-default python.version {[python_get_version]}
+default python.version         {[python_get_version]}
 
-# see #34562
+# see https://trac.macports.org/ticket/34562
 options python.consistent_destroot
 default python.consistent_destroot yes
 
 proc python_get_version {} {
     if {[string match py-* [option name]]} {
-        return [string range [option subport] 2 3]
+        return [string range [option subport] 2 [string first "-" [option subport]]-1]
     } else {
         return [option python.default_version]
     }
@@ -85,6 +87,17 @@ proc python_get_default_version {} {
     }
 }
 
+proc python_set_env_compilers {phase} {
+    if {[option supported_archs] eq "noarch"} {
+        return
+    }
+    foreach tag [option compwrap.compilers_to_wrap] {
+        if {[option configure.${tag}] ne ""} {
+            ${phase}.env-append [string toupper $tag]=[compwrap::wrap_compiler ${tag}]
+        }
+    }
+}
+
 proc python_set_versions {option action args} {
     if {$action ne "set"} {
         return
@@ -92,8 +105,7 @@ proc python_set_versions {option action args} {
     global name subport python._addedcode
     if {[string match py-* $name]} {
         foreach v [option $option] {
-
-            subport py${v}[string trimleft $name py] { depends_lib-append port:python${v} }
+            subport py${v}[string trimleft $name py] {}
         }
         if {$subport eq $name || $subport eq ""} {
             # Ensure the stub port does not do anything with distfiles—not
@@ -113,11 +125,10 @@ proc python_set_versions {option action args} {
             }
             extract {}
 
-            # set up py-foo as a stub port that depends on the default pyXY-foo
+            # set up py-foo as a stub port
             supported_archs noarch
-            global python.default_version python.version
+            global python.version
             unset python.version
-            depends_lib port:py${python.default_version}[string trimleft $name py]
             patch {}
             build {}
             destroot {
@@ -174,11 +185,7 @@ proc python_set_versions {option action args} {
                 build.env-append        OBJCFLAGS=$pyobjcflags
             }
             if {${python.set_compiler}} {
-                foreach var {cc objc cxx fc f77 f90} {
-                    if {[set configure.${var}] ne ""} {
-                        build.env-append [string toupper $var]=[set configure.${var}]
-                    }
-                }
+                python_set_env_compilers build
             }
         }
         pre-destroot {
@@ -225,11 +232,7 @@ proc python_set_versions {option action args} {
                 destroot.env-append     OBJCFLAGS=$pyobjcflags
             }
             if {${python.set_compiler} && ${python.consistent_destroot}} {
-                foreach var {cc objc cxx fc f77 f90} {
-                    if {[set configure.${var}] ne ""} {
-                        destroot.env-append [string toupper $var]=[set configure.${var}]
-                    }
-                }
+                python_set_env_compilers destroot
             }
         }
         post-destroot {
@@ -256,43 +259,118 @@ proc python_set_default_version {option action args} {
     if {$action ne "set"} {
         return
     }
-    global name subport python.default_version
-    if {[string match py-* $name]} {
-        if {$subport eq $name || $subport eq ""} {
-            depends_lib port:py${python.default_version}[string trimleft $name py]
-        }
-    } else {
-        python.versions ${python.default_version}
-        depends_lib-append port:python[option python.default_version]
+    if {![string match py-* [option name]]} {
+        python.versions [option python.default_version]
     }
 }
 
 
 options python.branch python.prefix python.bin python.lib python.libdir \
-        python.include python.pkgd
-# for pythonXY, python.branch is X.Y
-default python.branch   {[string range ${python.version} 0 end-1].[string index ${python.version} end]}
-# default python.prefix   {[python_get_defaults prefix]}
-# default python.bin      {[python_get_defaults bin]}
-# default python.lib      {[python_get_defaults lib]}
-# default python.pkgd     {[python_get_defaults pkgd]}
+        python.include python.pkgd python.pep517 python.pep517_backend \
+        python.add_dependencies
+# for pythonXY, python.branch is X.Y, for pythonXYZ, it's X.YZ
+default python.branch   {[string index ${python.version} 0].[string range ${python.version} 1 end]}
 default python.prefix   {${frameworks_dir}/Python.framework/Versions/${python.branch}}
 default python.bin      {${python.prefix}/bin/python${python.branch}}
 default python.lib      {${python.prefix}/Python}
 default python.pkgd     {${python.prefix}/lib/python${python.branch}/site-packages}
 default python.libdir   {${python.prefix}/lib/python${python.branch}}
 default python.include  {[python_get_defaults include]}
+default build.cmd       {[python_get_defaults build_cmd]}
+default build.target    {[python_get_defaults build_target]}
+default destroot.cmd    {[python_get_defaults destroot_cmd]}
+default destroot.destdir {[python_get_defaults destroot_destdir]}
+default destroot.target {[python_get_defaults destroot_target]}
 
-# default build.cmd       {"${python.bin} setup.py [python_get_defaults setup_args]"}
-# default destroot.cmd    {"${python.bin} setup.py [python_get_defaults setup_args]"}
-# default destroot.destdir {"--prefix=[python_get_defaults setup_prefix] --root=${destroot}"}
-default build.cmd       {${python.bin} setup.py --no-user-cfg}
-default destroot.cmd    {${python.bin} setup.py --no-user-cfg}
-default destroot.destdir {--prefix=${python.prefix} --root=${destroot}}
+default python.pep517   no
+default python.pep517_backend   setuptools
+
+default python.add_dependencies yes
+proc python_add_dependencies {} {
+    if {[option python.add_dependencies]} {
+        global subport python.version python.default_version
+        if {[string match py-* $subport]} {
+            # set up py-foo as a stub port that depends on the default pyXY-foo
+            depends_lib-delete port:py${python.default_version}[string trimleft $subport py]
+            depends_lib-append port:py${python.default_version}[string trimleft $subport py]
+        } else {
+            depends_lib-delete port:python${python.version}
+            depends_lib-append port:python${python.version}
+            if {[option python.pep517]} {
+                depends_build-delete    port:py${python.version}-build \
+                                        port:py${python.version}-python-install
+                depends_build-append    port:py${python.version}-build \
+                                        port:py${python.version}-python-install
+                switch -- [option python.pep517_backend] {
+                    setuptools {
+                        depends_build-delete    port:py${python.version}-setuptools \
+                                                port:py${python.version}-wheel
+                        depends_build-append    port:py${python.version}-setuptools \
+                                                port:py${python.version}-wheel
+                    }
+                    flit {
+                        depends_build-delete    port:py${python.version}-flit_core
+                        depends_build-append    port:py${python.version}-flit_core
+                    }
+                    poetry {
+                        depends_build-delete    port:py${python.version}-poetry-core
+                        depends_build-append    port:py${python.version}-poetry-core
+                    }
+                    default {}
+                }
+            }
+        }
+    }
+}
+port::register_callback python_add_dependencies
+
 
 proc python_get_defaults {var} {
-    global python.version python.branch prefix python.prefix os.platform
+    global python.version python.branch python.prefix python.bin python.pep517 workpath
     switch -- $var {
+        binary_suffix {
+            if {[string match py-* [option name]]} {
+                return -${python.branch}
+            } else {
+                return ""
+            }
+        }
+        build_cmd {
+            if {${python.pep517}} {
+                return "${python.bin} -m build --wheel --no-isolation --outdir ${workpath}"
+            } else {
+                return "${python.bin} setup.py --no-user-cfg"
+            }
+        }
+        build_target {
+            if {${python.pep517}} {
+                return ""
+            } else {
+                return build[python_get_defaults jobs_arg]
+            }
+        }
+        destroot_cmd {
+            if {${python.pep517}} {
+                return "${python.bin} -m install --verbose"
+            } else {
+                return "${python.bin} setup.py --no-user-cfg"
+            }
+        }
+        destroot_destdir {
+            global destroot
+            if {${python.pep517}} {
+                return "--destdir ${destroot}"
+            } else {
+                return "--prefix=${python.prefix} --root=${destroot}"
+            }
+        }
+        destroot_target {
+            if {${python.pep517}} {
+                return [glob -nocomplain -directory ${workpath} *.whl]
+            } else {
+                return install
+            }
+        }
         include {
             set inc_dir "${python.prefix}/include/python${python.branch}"
             if {[file exists ${inc_dir}]} {
@@ -300,7 +378,7 @@ proc python_get_defaults {var} {
             } else {
                 # look for "${inc_dir}*" and pick the first one found;
                 # make assumptions if none are found
-                if {[catch {set inc_dirs [glob ${inc_dir}*]}]} {
+                if {[catch {glob ${inc_dir}*} inc_dirs]} {
                     # append 'm' suffix if 30 <= PyVer <= 37
                     # Py27- and Py38+ do not use this suffix
                     if {${python.version} < 30 || ${python.version} > 37} {
@@ -314,17 +392,11 @@ proc python_get_defaults {var} {
             }
         }
         pkgd {
+		  global prefix
             if {${python.version} != 24} {
                 return "${python.prefix}/lib/python${python.branch}/site-packages"
             } else {
                 return "${prefix}/lib/python${python.branch}/site-packages"
-            }
-        }
-        binary_suffix {
-            if {[string match py-* [option name]]} {
-                return -${python.branch}
-            } else {
-                return ""
             }
         }
         jobs_arg {
