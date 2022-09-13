c11bb37cd92b030cbdb49a2b3125cb7e4a1a68dd
René Bertin <rjvbertin@gmail.com>
2022-09-13 18:27:24 +0200

    port:pkgconfig : proxying the system pkg-config now only fetches the system's default search path
    Committed from host : Bola

--- devel/pkgconfig/files/pkg-config.sh
+++ devel/pkgconfig/files/pkg-config.sh
@@ -2,14 +2,16 @@
 
 if [ "${PKG_CONFIG_MACPORTS_ONLY}" = "" ] ;then
 	if [ "${PKG_CONFIG}" = "" -o "${PKG_CONFIG}" = $0 ] ;then
-		PKG_CONFIG_SYS="/usr/bin/pkg-config"
+		PKG_CONFIG_SYS="pkg-config-mp"
+		SYSPKG_PATH="`/usr/bin/pkg-config --variable pc_path pkg-config`"
 	else
 		PKG_CONFIG_SYS="${PKG_CONFIG}"
+		SYSPKG_PATH="`${PKG_CONFIG_SYS} --variable pc_path pkg-config`"
 	fi
 	if [ "${PKG_CONFIG_PATH}" != "" ] ;then
-		PKG_CONFIG_PATH="@PREFIX@/lib/pkgconfig:@PREFIX@/share/pkgconfig:@PREFIX@/lib/@BUILDARCH@-linux-gnu/pkgconfig:${PKG_CONFIG_PATH}"
+		PKG_CONFIG_PATH="@PREFIX@/lib/pkgconfig:@PREFIX@/share/pkgconfig:@PREFIX@/lib/@BUILDARCH@-linux-gnu/pkgconfig:${PKG_CONFIG_PATH}:${SYSPKG_PATH}"
 	else
-		PKG_CONFIG_PATH="@PREFIX@/lib/pkgconfig:@PREFIX@/share/pkgconfig:@PREFIX@/lib/@BUILDARCH@-linux-gnu/pkgconfig"
+		PKG_CONFIG_PATH="@PREFIX@/lib/pkgconfig:@PREFIX@/share/pkgconfig:@PREFIX@/lib/@BUILDARCH@-linux-gnu/pkgconfig:${SYSPKG_PATH}"
 	fi
 	export PKG_CONFIG_PATH
 	exec "${PKG_CONFIG_SYS}" "$@"
