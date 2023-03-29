8f720d27eb0bce54da7d124960534375695156b0
René Bertin <rjvbertin@gmail.com>
2023-03-29 15:21:41 +0200

    port:dbus : upgrade to 1.14.6
    Committed from host : Bola

--- devel/dbus/files/patch-autogen.sh
+++
@@ -1,9 +0,0 @@
---- orig.autogen.sh	2015-09-30 16:48:40.000000000 +0200
-+++ autogen.sh	2016-09-20 20:33:50.000000000 +0200
-@@ -101,5 +104,5 @@
- fi
- 
- if $run_configure; then
--    $srcdir/configure --enable-developer --config-cache "$@"
-+    $srcdir/configure "$@"
- fi
